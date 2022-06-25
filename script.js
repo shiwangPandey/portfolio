fda827b2fc1947f750d5b6af5ee0969449bb4b74
shiwang pandey <97517610+shiwangPandey@users.noreply.github.com>
2022-06-25 06:52:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Script running...")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(()=>{
+
+       
+                document.querySelector('.cross').style.display = 'inline'
+        },300);
+    }
+})
+function myfunc(event) {
+    event.preventDefault();
+
+    var clientname = document.getElementById("clientname").value;
+    var clientmail = document.getElementById("clientmail").value;
+    var clientphone = document.getElementById("clientphone").value;
+    var clientenquiry = document.getElementById("clientenquiry").value;
+    
+    localStorage.setItem('Name', clientname);
+    localStorage.setItem('Email', clientmail );
+    localStorage.setItem('Phone No.', clientphone );
+    localStorage.setItem('Enquiry', clientenquiry);
+
+}
